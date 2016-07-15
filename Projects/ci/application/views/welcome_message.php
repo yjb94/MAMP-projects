07d8f04c955c9f67e2f2ab52463581ea2db5ff34
Camo_u <jbyun94@naver.com>
2016-07-16 04:23:30 +0900

    added bg, logo, top menu(semi)

--- Projects/ci/application/views/welcome_message.php
+++ Projects/ci/application/views/welcome_message.php
@@ -4,86 +4,76 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>CAMOUFLAGE and STYLE</title>
 
 	<style type="text/css">
 
 	::selection { background-color: #E13300; color: white; }
 	::-moz-selection { background-color: #E13300; color: white; }
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
+	body
+	{
+		/*background-image: url('https://images.unsplash.com/photo-1465326117523-6450112b60b2?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=f340b4d4a364ac53dcb92955373fc5e0');*/
+		background-image: url('http://localhost:8888/bg.jpg');
+		background-size: 100%;
+		background-color: #dddddd;
+		margin: 0px;
 	}
 
-	a {
-		color: #003399;
-		background-color: transparent;
+	/*a:link    { color: white; text-decoration: none; }*/
+	/*a:visited { color: white; text-decoration: none; }*/
+	/*a:hover   { color: white; text-decoration: none; }*/
+
+	.top_bar
+	{
+		top: 0px;
+		margin: 0px;
+		width: 100%;
+		height: 100px;
+		position: fixed;
+	}
+	.menu_box
+	{
+		width: 100%;
+		height: 85px;
+		background-color: white;
+		opacity: 0.75;
+		position: absolute;
+		top: 10px;
+	}
+	.logo_box
+	{
+		margin-left: 8%;
+		background-color: #333333;
+		position: absolute;
+		width: 120px;
+		height: 70px;
+		text-align: center;
+		padding-top: 10px;
+	}
+	.logo_text
+	{
+		position: absolute;
+		top:50px;
+		left:22px;
+		color: white;
 		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
+		font-family: "herculanum", herculanum, fantasy;
+		font-size: 18px;
+		margin:0px;
 	}
 	</style>
 </head>
 <body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+<div class="top_bar">
+	<div class="menu_box">
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<a href="">
+		<div class="logo_box">
+			<img src="http://localhost:8888/camo_u_white.png" style="width: 70px; height:auto;">
+			<h1 class="logo_text">camo_u</h1>
+		</div>
+	</a>
 </div>
-
 </body>
 </html>
